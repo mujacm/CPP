2f1b9b11c41d85f887b77764b1cfd922cedeeebf
siddkataria <siddhesh.199302112@muj.manipal.edu>
2020-10-27 21:27:16 +0530

    Files Added

---
+++ Programs/encryption.cpp
@@ -0,0 +1,39 @@
+/*
+Problem: To encrypt and decrypt a message by manual key. Basically, to create a secret message for communication.
+*/
+
+#include <iostream>
+#include <math.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+    string key{ "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM" };
+    string msg;
+    cout << "Enter your secret message: ";
+    getline(cin, msg);
+    cout << endl << "Encrypting..." << endl;
+    string encrypt = {};
+    for (int i=0;i<msg.length();i++)
+    {
+        for(int j=0;j<alphabet.length();j++)
+        {
+            if(alphabet[j]==msg[i])
+            msg[i]=key[j];
+        }
+    }
+    cout<<"Encrypted message is: "<<msg;
+    cout<<endl<<"Decrypting...";
+    for (int i=0;i<msg.length();i++)
+    {
+        for(int j=0;j<key.length();j++)
+        {
+            if(key[j]==msg[i])
+            msg[i]=alphabet[j];
+        }
+    }
+    cout<<endl<<"Decrypted message is: "<<msg;
+    return 0;
+}
---
+++ Programs/sum.cpp
@@ -0,0 +1,15 @@
+/*
+Problem: To print sum of first n numbers.
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+        int n,s=0;
+        cout<<"Enter a number: ";
+        cin>>n;
+        for(int i=1;i<=n;i++)
+           s=s+i;
+        cout<<endl<<"Sum of 1 to "<<n<<" is = "<<s;
+    return 0;
+}
