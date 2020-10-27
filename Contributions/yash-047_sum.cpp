31d3a95c140d33edc8a018f42ad95ef6dfec9875
yash-047 <73501217+yash-047@users.noreply.github.com>
2020-10-28 02:41:11 +0530

    Fixed line 8 and improved spacing
    Fixed the issue with endl in line 8 of original program (line 21 in the fixed code)

---
+++ Contributions/yash-047_sum.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: To print sum of first n numbers.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        
+        int n,s = 0;
+        
+        cout << "Enter a number: ";
+        cin >> n;
+        
+        for (int i = 1; i <= n; i++)
+        {
+            s = s + i;
+        }
+
+        cout << "Sum of 1 to "<< n << "is = " << s << endl;
+    
+     return 0;
+}
