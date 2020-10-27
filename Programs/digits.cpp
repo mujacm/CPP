dc49ae29868a5402a77e649ccae8cafe07131301
AryamanS <73508676+AryamanS@users.noreply.github.com>
2020-10-27 13:28:38 -0700

    Function
    added a function number of digits

--- Programs/digits.cpp
+++ Programs/digits.cpp
@@ -5,18 +5,22 @@ Problem: To find number of digits of a number.
 #include <iostream>
 #include <math.h>
 using namespace std;
-int main() {
-            int n;
-            cout<<"Enter a number: ";
-            cin>>n;
-            int t=n;
-            int d=0;
-            while(t!=0)
+void numberofdigits(int n)
+{
+     int d=0;
+     int t=n;
+    while(t!=0)
             {
                     t=t/10;
                     d++;
             }
             cout<<endl<<"Number of digits of "<<n<<" is "<<d;
+}
+int main() {
+            int n;
+            cout<<"Enter a number: ";
+            cin>>n;
+            numberofdigits(n);
         return 0;
 }
 
