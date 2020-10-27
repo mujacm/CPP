05102586e3003a3eb4061cfc6a3b17a11c7ace5d
yoloabhi <abhirup_cool2@yahoo.com>
2020-10-28 01:28:09 +0530

    Created C++

---
+++ Contributions/digits.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: To find number of digits of a number.
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+            int n;
+            cout<<"Enter a your number: ";
+            cin>>n;
+            int t=n;
+            int d=0;
+            while(t!=0)
+            {
+                    t=t/10;
+                    d++;
+            }
+            cout<<endl<<"Number of digits of "<<n<<" is "<<d;
+        return 0;
+}
+
+
