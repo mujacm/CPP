39791d80dfe64907ddb195dd3e8595fc6d27bbc6
AryamanS <73508676+AryamanS@users.noreply.github.com>
2020-10-27 13:04:07 -0700

    Update sum.cpp
    Added a function sum to perform the same operation.

--- Programs/sum.cpp
+++ Programs/sum.cpp
@@ -4,12 +4,17 @@ Problem: To print sum of first n numbers.
 
 #include <iostream>
 using namespace std;
-int main() {
-        int n,s=0;
-        cout<<"Enter a number: ";
-        cin>>n;
+void sum(int n)
+{
+         int s=0;
         for(int i=1;i<=n;i++)
            s=s+i;
         cout<<endl<<"Sum of 1 to "<<n<<" is = "<<s;
+}
+int main() {
+        
+        cout<<"Enter a number: ";
+        cin>>n;
+        sum(n);
     return 0;
 }
