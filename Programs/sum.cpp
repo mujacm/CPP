8c4a9b7d3673a32f5e9655e82775135ef6640835
AryamanS <73508676+AryamanS@users.noreply.github.com>
2020-10-27 13:16:05 -0700

    Added a function
    Sum function performs the same operation

--- Programs/sum.cpp
+++ Programs/sum.cpp
@@ -4,12 +4,17 @@ Problem: To print sum of first n numbers.
 
 #include <iostream>
 using namespace std;
+void sum(int n )
+{
+		int s=0;
+        for(int i=1;i<=n;i++)
+           s=s+i;
+        cout<<endl<<"Sum of 1 to "<<n<<" is = "<<s;
+}
 int main() {
         int n,s=0;
         cout<<"Enter a number: ";
         cin>>n;
-        for(int i=1;i<=n;i++)
-           s=s+i;
-        cout<<endl<<"Sum of 1 to "<<n<<" is = "<<s;
+       	sum(n);
     return 0;
 }
