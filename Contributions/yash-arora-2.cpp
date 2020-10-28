e53233f87eec02b63e3e99ed42b8936abcabb270
yashar1908 <73465642+yashar1908@users.noreply.github.com>
2020-10-28 18:18:43 +0530

    added a program

---
+++ Contributions/yash-arora-2.cpp
@@ -0,0 +1,19 @@
+//program to take two numbers as input and return their sum, and product.
+
+#include <iostream>
+#include <math.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	cout<<"Enter the first number: ";
+	cin>>a;
+	cout<<"Enter the second number: ";
+	cin>>b;
+	cout<<"The sum of the two numbers is: "<<a+b<<endl;
+	cout<<"The product of the two numbers is: "<<a*b<<endl;
+	return 0;
+}
