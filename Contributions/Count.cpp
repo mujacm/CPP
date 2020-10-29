387ebef2c548aa6912c1a59275c1ff0953f60646
apuneet839 <apuneet839@gmail.com>
2020-10-29 18:11:51 +0530

    Count number of digits

---
+++ Contributions/Count.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: To find number of digits of a number.
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+            int n;
+            cout<<"Enter a number: ";
+            cin>>n;
+            int t=n;
+            int d=0;
+            while(t!=0)
+            {
+                    t=t/10;
+                    d++;
+            }
+            cout<<endl<<"Number of digits "<<n<<" is ";
+        return 0;
+}
